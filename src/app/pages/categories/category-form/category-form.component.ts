ccbee2b1c5c5ddc282165826a36c76fc51c3df43
Jorge <jorge_aquino@hotmail.com.br>
2019-05-26 23:04:29 -0300

    finalizando o modulo de categorias

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -128,7 +128,7 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
     if (error.status === 422) {
       this.serverErrorMessages = JSON.parse(error._body).errors;
     } else {
-      this.serverErrorMessages = ['Falaha na comunicação com o servidor, por favor tente mais tarde.'];
+      this.serverErrorMessages = ['Falha na comunicação com o servidor, por favor tente mais tarde.'];
     }
   }
 }
