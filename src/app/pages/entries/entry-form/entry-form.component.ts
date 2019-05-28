647deb12f063964ca8fe465f4847d789822bdc9a
Jorge <jorge_aquino@hotmail.com.br>
2019-05-28 00:44:11 -0300

    improving EntryFormComponent

--- src/app/pages/entries/entry-form/entry-form.component.ts
+++ src/app/pages/entries/entry-form/entry-form.component.ts
@@ -8,6 +8,8 @@ import { EntryService } from '../shared/entry.service';
 import { switchMap } from 'rxjs/operators';
 
 import toastr from 'toastr';
+import { Category } from '../../categories/shared/category.model';
+import { CategoryService } from './../../categories/shared/category.service';
 
 @Component({
   selector: 'app-entry-form',
@@ -23,6 +25,7 @@ export class EntryFormComponent implements OnInit, AfterContentChecked {
   // tslint:disable-next-line:no-inferrable-types
   submittingForm: boolean = false;
   entry: Entry = new Entry();
+  categories: Array<Category>;
 
   imaskConfig = {
     mask: Number,
@@ -30,20 +33,36 @@ export class EntryFormComponent implements OnInit, AfterContentChecked {
     thousandsSeparator: '',
     padFractionalZeros: true,
     normalizeZeros: true,
-    radix: '.',
+    radix: ',',
+  };
+
+  ptBR = {
+    firstDayOfWeek: 0,
+    dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
+    dayNamesShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'],
+    dayNamesMin: ['Do', 'Se', 'Te', 'Qu', 'Qu', 'Se', 'Sa'],
+    monthNames: [
+      'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho',
+      'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'
+    ],
+    monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+    today: 'Hoje',
+    clear: 'Limpar'
   };
 
   constructor(
     private entryService: EntryService,
     private route: ActivatedRoute,
     private router: Router,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private categoryService: CategoryService
   ) { }
 
   ngOnInit() {
     this.setCurrentAction();
     this.buildEntryForm();
     this.loadEntry();
+    this.loadCategories();
   }
 
   ngAfterContentChecked() {
@@ -60,6 +79,17 @@ export class EntryFormComponent implements OnInit, AfterContentChecked {
     }
   }
 
+  get typeOptions(): Array<any> {
+    return Object.entries(Entry.types).map(
+      ([valuee, textt]) => {
+        return {
+          text: textt,
+          value: valuee
+        };
+      }
+    );
+  }
+
   // private methods
   private setCurrentAction() {
     if (this.route.snapshot.url[0].path === 'new') {
@@ -74,10 +104,10 @@ export class EntryFormComponent implements OnInit, AfterContentChecked {
       id: [null],
       name: [null, [Validators.required, Validators.minLength(2)]],
       description: [null],
-      type: [null, [Validators.required]],
+      type: ['expense', [Validators.required]],
       amount: [null, [Validators.required]],
       date: [null, [Validators.required]],
-      paid: [null, [Validators.required]],
+      paid: [true, [Validators.required]],
       categoryId: [null, [Validators.required]]
     });
   }
@@ -94,6 +124,12 @@ export class EntryFormComponent implements OnInit, AfterContentChecked {
         alert('Ocorreu um erro no servidor, tente mais tarde.');
       });
     }
+  }
+
+  private loadCategories() {
+    this.categoryService.getAll().subscribe((categories) => {
+      this.categories = categories;
+    });
   }
 
   private setPageTitle() {
