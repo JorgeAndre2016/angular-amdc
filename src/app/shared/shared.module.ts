535cd8e4488658f262d89c9614e09e34190713c2
Jorge <jorge_aquino@hotmail.com.br>
2019-06-09 23:50:09 -0300

    desenvolvimento componente para apresentação de erros de formulários

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,11 +4,13 @@ import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component';
 import { PageHeaderComponent } from './components/page-header/page-header.component';
+import { FrmFieldErrorComponent } from './components/frm-field-error/frm-field-error.component';
 
 @NgModule({
   declarations: [
     BreadCrumbComponent,
     PageHeaderComponent,
+    FrmFieldErrorComponent,
   ],
   imports: [
     CommonModule,
@@ -21,6 +23,7 @@ import { PageHeaderComponent } from './components/page-header/page-header.compon
     ReactiveFormsModule,
     BreadCrumbComponent,
     PageHeaderComponent,
+    FrmFieldErrorComponent,
   ]
 })
 export class SharedModule { }
