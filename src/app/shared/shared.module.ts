fe8b361d8ce464b1f7f6b3f82b1b91377a24303c
Jorge <jorge_aquino@hotmail.com.br>
2019-06-09 22:57:34 -0300

    componentização do header

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component';
+import { PageHeaderComponent } from './components/page-header/page-header.component';
 
 @NgModule({
   declarations: [
-    BreadCrumbComponent
+    BreadCrumbComponent,
+    PageHeaderComponent,
   ],
   imports: [
     CommonModule,
@@ -18,6 +20,7 @@ import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.compon
     RouterModule,
     ReactiveFormsModule,
     BreadCrumbComponent,
+    PageHeaderComponent,
   ]
 })
 export class SharedModule { }
